c35b2d5ef0f3542643b0e7f6d30e76b89828e0a4
Thomas Jacquelinet <tjacquel@made-f0Ar7s11.clusters.42paris.fr>
2024-09-26 21:50:51 +0200

    v2

---
+++ get_next_line.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjacquel <tjacquel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 18:04:15 by tjacquel          #+#    #+#             */
+/*   Updated: 2024/09/26 21:42:19 by tjacquel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+
+char	*ft_read_line(int fd, char *string, char *buffer)
+{
+	char	*tmp;
+	int		bytes_read;
+
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(string);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		if (string == NULL)
+			string = ft_strdup("");
+		tmp = string;
+		string = ft_strjoin(tmp, buffer);
+		free(tmp);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (string);
+}
+
+char	*ft_extract_line(char *string)
+{
+	char	*line;
+	int		i;
+
+	if (!string || !*string)
+		return (NULL);
+	//i = 0;
+	//while (string[i] != '\0' && string[i] != '\n')
+	//	i++;
+	line = malloc((ft_strlen(string) + 2) * sizeof(char));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (string[i] != '\0' && string[i] != '\n')
+	{
+		line[i] = string[i];
+		i++;
+	}
+	if (string[i] == '\n')
+	{
+		line[i] = '\n';
+		i++;	
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_update_stash(char *string)
+{
+	char	*new_stash;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (string[i] != '\0' && string[i] != '\n')
+		i++;
+	if (string[i] == '\0')
+	{
+		free(string);
+		return (NULL);
+	}
+	new_stash = malloc((ft_strlen(string) - i + 1) * sizeof(char));
+	if (!new_stash)
+		return (NULL);
+	i++;
+	j = 0;
+	while (string[i] != '\0')
+		new_stash[j++] = string[i++];
+	new_stash[j] = '\0';
+	free(string);
+	return (new_stash);
+}
+
+// Main function: Get the next line from the file descriptor
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+	char		*buffer;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	stash = ft_read_line(fd, stash, buffer);
+	free(buffer);
+	if (!stash)
+		return (NULL);
+	line = ft_extract_line(stash);
+	stash = ft_update_stash(stash);
+	return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjacquel <tjacquel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 15:37:32 by tjacquel          #+#    #+#             */
+/*   Updated: 2024/09/26 19:14:49 by tjacquel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_update_stash(char *string);
+char	*ft_extract_line(char *string);
+char	*ft_read_line(int fd, char *string, char *buffer);
+int		ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strchr(const char *s, int c);
+
+#endif
